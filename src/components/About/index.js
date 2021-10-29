cefb0939d71544ccb7e5684b90db3554ce6bbe64
Harborth, Allen <allenharborth9835@gmail.com>
2021-10-28 21:05:25 -0500

    add about-me and nav componets

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,7 @@ const About = () =>{
   return (
     <section className="my-5">
       <h1 id="about">Who am I?</h1>
-      <img src={coverIamge} className="my-2" style={{ width: "100%"}} alt="cover" />
+      <img src={coverImage} className="my-2" style={{ width: "100%"}} alt="cover" />
     </section>
   );
 }
